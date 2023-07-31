b6a1534eeb642b8c3b2472d5e2e101d91b857e01
Pau Velasco <pauvelascogarrofe@gmail.com>
2023-07-31 18:44:53 +0200

    create TextArea component

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,56 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Container, Col, Row, Button } from 'react-bootstrap'
+import { Container, Col, Row, Button, Stack } from 'react-bootstrap'
 import { useStore } from './hooks/useStore'
 import { AUTO_LANGUAGE } from './consts'
 import { ArrowsIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { SectionType } from './types.d'
+import { TextArea } from './components/TextArea'
 
 function App () {
-  const { fromLanguage, toLanguage, interchangeLanguages } = useStore()
+  const { loading, fromLanguage, toLanguage, interchangeLanguages, setFromLanguage, setToLanguage, fromText, result, setFromText, setResult } = useStore()
 
   return (
 
     <Container fluid>
+      <div>
+        <h2>GOOGLE TRANSLATE</h2>
+      </div>
       <Row>
         <Col>
-          <h2>From</h2>
-          {fromLanguage}
+          <Stack gap={2}>
+            <LanguageSelector
+              onChange={ setFromLanguage }
+              type={ SectionType.From}
+              value={ fromLanguage }
+            />
+            <TextArea
+              type={ SectionType.From }
+              value={ fromText }
+              onChange={ setFromText }
+            />
+          </Stack>
         </Col>
-        <Col>
+        <Col xs='auto'>
           <Button variant='link' disabled={fromLanguage === AUTO_LANGUAGE} onClick={interchangeLanguages }><ArrowsIcon/></Button>
         </Col>
         <Col>
-          <h2>To</h2>
-          {toLanguage}
+          <Stack gap={2}>
+            <LanguageSelector
+              onChange={ setToLanguage }
+              type={ SectionType.To}
+              value={ toLanguage }
+            />
+            <TextArea
+              type={ SectionType.To }
+              loading={ loading}
+              value={ result}
+              onChange={ setResult }
+            />
+          </Stack>
         </Col>
       </Row>
-      <div>
-        <h1>GOOGLE TRANSLATE</h1>
-      </div>
     </Container>
 
   )
